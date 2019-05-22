5809980a9359a1c7416501d23e6404f82f70db46
Chad\YOGA <glinskyc@gmail.com>
2019-05-22 14:04:26 -0600

    Updates template notebook, including an example module and image file.

---
+++ template_module.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Drivetrain Hub LLC
+# For non-commercial use only.  For commercial products and services, visit https://www.drivetrainhub.com.
+
+"""Notebook module for notebooks template."""
+
+
+def divide(x, y):
+    """Divide two numbers.
+
+    :param x: Numerator value.
+    :param y: Denominator value.
+    :return: Quotient of x and y.
+    """
+
+    return x / y
